9f6ecdc192018316a6eed2010a08601956d0055b
sidahmed <ahorob57@gmail.com>
2021-06-09 04:17:42 +0100

    request response model

---
+++ client.js
@@ -0,0 +1,21 @@
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+
+const packageDef = protoLoader.loadSync('todo.proto', { })
+const grpcObject = grpc.loadPackageDefinition(packageDef)
+const todoPackage = grpcObject.todoPackage
+
+const client = new todoPackage.Todo('localhost:4000', grpc.credentials.createInsecure())
+
+const text = process.argv[2]
+
+client.createTodo({
+    "id": -1,
+    "text": text
+}, (err, res) => {
+    console.log("recieved from server CreateTodo: "+ JSON.stringify(res))
+})
+
+client.readTodos({}, (err, res) => {
+    console.log("recieved from server ReadTodo: "+ JSON.stringify(res))
+})
